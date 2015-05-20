4c03863cc540ac0b2778cda12a4a58f69005722a
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-05-20 15:24:48 -0400

    Cleanup docs by topics

--- topicmodelling.R
+++ topicmodelling.R
@@ -6,11 +6,6 @@ library(readr)
 choice <- mallet.read.dir("editors-choice/")
 inputs <- mallet.read.dir("inputs/")
 
-stops <- stopwords("english")
-stops_file <- file("stopwords.txt")
-writeLines(stops, stops_file)
-close(stops_file)
-
 inst <- mallet.import(c(choice$id, inputs$id),
                       c(choice$text, inputs$text),
                       "stopwords.txt")
@@ -19,16 +14,6 @@ topic_model <- MalletLDA(num.topics = 40)
 topic_model$loadDocuments(inst)
 topic_model$setAlphaOptimization(20, 50)
 
-
-freq <- mallet.word.freqs(topic_model)
-freq %>%
-  arrange(-term.freq) %>%
-  top_n(20)
-
-freq %>%
-  arrange(-doc.freq) %>%
-  top_n(20)
-
 topic_model$train(200)
 topic_model$maximize(10)
 
@@ -36,11 +21,7 @@ doc.topics <- mallet.doc.topics(topic_model, smoothed = T, normalized = T)
 
 class <- rep("nonchoice", length = nrow(doc.topics))
 class[1:length(choice$id)] <- "choice"
-
 filenames <- c(choice$id, inputs$id)
-
 doc.topics <- cbind(doc.topics, class, filenames)
-
-View(doc.topics)
-
+doc.topics <- as.data.frame(doc.topics)
 write_csv(doc.topics, "docs-by-topics.csv")
